94cda1ee28b29a981172ccce6a78f71d0465c028
Nurbol-Xan <nurbolxaydaraliyev007@gmail.com>
2022-08-04 14:50:46 +0500

    PostgeSQL to SQLite

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ ruby "3.1.2"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
 # Use postgresql as the database for Active Record
-gem "pg", "~> 1.1"
+gem 'sqlite3'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_02_115021) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "photos", force: :cascade do |t|
     t.bigint "albumId"
     t.string "title"
